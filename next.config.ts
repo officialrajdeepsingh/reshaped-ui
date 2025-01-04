9d5cf6d433fa963e8a46a5b3bd0ca41603319467
Rajdeep Singh <radewala.2@gmail.com>
2025-01-04 22:22:08 +0530

    config the reshaped ui for optimze

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+/* config options here */
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["reshaped"],
+  experimental: {
+    optimizePackageImports: ["reshaped"],
+  },
 };
 
 export default nextConfig;
