87fa299c7256edc81eeb7f9711c6503be2b68a46
Rajdeep Singh <radewala.2@gmail.com>
2025-01-05 17:00:41 +0530

    rename the file name APP to Wrap

--- components/Wrap.tsx
+++ components/Wrap.tsx
@@ -3,7 +3,7 @@
 import type { ReactNode } from "react";
 import { Reshaped } from "reshaped";
 
-export default function App({ children }: { children: ReactNode }) {
+export default function Wrap({ children }: { children: ReactNode }) {
   return <Reshaped defaultColorMode="dark" theme="reshaped">{children}</Reshaped>;
 };
 
