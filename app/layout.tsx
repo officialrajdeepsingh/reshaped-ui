358bc3f3a03358fb70610c413e0290bdec808a5a
Rajdeep Singh <radewala.2@gmail.com>
2025-01-05 17:01:14 +0530

    fix the Wrap component with import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import App from "./App";
+import Wrap from "@/components/Wrap";
 import "reshaped/themes/reshaped/theme.css";
 import "./globals.css";
-
 import { Header } from "@/components/Header";
 
 const geistSans = Geist({
@@ -25,10 +24,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
-         <App> 
+         <Wrap> 
           <Header />
              {children} 
-         </App>
+         </Wrap>
       </body>
     </html>
   );
